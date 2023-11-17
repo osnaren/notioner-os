54fd557a0105cec4d72c1f054fea521e8aef2d0d
Obuli Sai Naren <66naren@gmail.com>
2023-11-17 08:21:10 +0530

    feat: 🎉 Add authentication middleware and constants
    This commit adds a new file `authenticate.js` that contains the implementation of an authentication middleware. It also introduces a new file `constants.js` that defines an array of paths to be exempted from authentication.
    
    The `authenticate` middleware checks if the request path is in the exceptions list defined in `constants.js`. If it is, the middleware bypasses authentication and proceeds to the next middleware. Otherwise, it checks for a valid token in the request headers. If no token is provided or if the token format is invalid, it returns an appropriate error response. If a valid token is found, it verifies and decodes it using the JWT secret key stored in an environment variable. The decoded token is then added to the request object before proceeding to the next middleware.
    
    These changes were made to enhance security by implementing proper authentication for protected routes.
    
    Note: The actual values of `JWT_SECRET_KEY` and `AUTH_EXCEPT_PATHS` are not included here for security reasons.

---
+++ utils/authenticate.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+const { AUTH_EXCEPT_PATHS } = require("./constants");
+const JWT_SECRET = process.env.JWT_SECRET_KEY;
+
+const authenticate = (req, res, next) => {
+  // Check if the request path is in the exceptions list
+  if (AUTH_EXCEPT_PATHS.includes(req.path)) {
+    return next(); // Bypass authentication
+  }
+
+  let token = req.headers["authorization"] || req.headers["Authorization"];
+  if (token) {
+    const tokenParts = token.split(" ");
+    if (tokenParts.length === 2 && tokenParts[0] === "Bearer") {
+      token = tokenParts[1];
+    } else {
+      return res.status(401).send("Invalid token format.");
+    }
+  } else {
+    return res.status(401).send("Access denied. No token provided.");
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded; // Add the decoded token to the request
+    next();
+  } catch (error) {
+    console.error(error); // Internal logging
+    res.status(403).send("Invalid token."); // Public response
+  }
+};
+
+module.exports = {
+  authenticate,
+};
---
+++ utils/constants.js
@@ -0,0 +1,5 @@
+const AUTH_EXCEPT_PATHS = ["/", "/favicon.ico"];
+
+module.exports = {
+  AUTH_EXCEPT_PATHS,
+};
