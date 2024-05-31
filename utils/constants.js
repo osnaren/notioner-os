9582c56f7d580c058b2a41b1fd31bf0b213ca7d6
Obuli Sai Naren <66naren@gmail.com>
2024-05-31 10:16:46 +0530

    feat: ✨ Add status page and styles
    Add status.html, status.css, and FETCH_STATUS constant for movie status tracking.

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,11 @@
-const AUTH_EXCEPT_PATHS = ["/", "/favicon.ico"];
+const AUTH_EXCEPT_PATHS = ["/", "/favicon.ico", "/status"];
+
+let FETCH_STATUS = {
+  lastFetched: null,
+  nextFetch: null,
+};
 
 module.exports = {
   AUTH_EXCEPT_PATHS,
+  FETCH_STATUS,
 };
