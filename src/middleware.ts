ba9edefb7ca2470142cff7816a5df872c41b4535
Obuli Sai Naren <66naren@gmail.com>
2024-07-19 21:38:32 +0530

    WIP

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+// src/middleware.ts
+import { NextRequest, NextResponse } from "next/server";
+import { verifyAuth } from "./middleware/auth";
+import { PROTECTED_ROUTES } from "@config/constants";
+
+export async function middleware(req: NextRequest) {
+  const url = req.nextUrl.clone();
+
+  // Check if the request path matches any of the protected routes
+  if (PROTECTED_ROUTES.some((route) => url.pathname.startsWith(route))) {
+    // Perform authentication check
+    const authResult = await verifyAuth(req);
+    if (authResult !== NextResponse.next()) {
+      return authResult;
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,9 +1,43 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
 
-export function middleware(req: NextRequest) {
-  const token = req.cookies.get('token');
-  if (!token) {
-    return NextResponse.redirect(new URL('/login', req.url));
+const JWT_SECRET = process.env.JWT_SECRET_KEY || "";
+
+const allowedIPs = process.env.ALLOWED_IPS?.split(",") || [];
+
+export async function verifyAuth(req: NextRequest) {
+  const requestIP = req.headers.get("x-forwarded-for") || req.ip || "127.0.0.1";
+  const { hostname, searchParams } = new URL(req.url);
+
+  const unauthorized =
+    !validateHostname(hostname) || !validateToken(searchParams.get("token") || "") || !validateIP(requestIP);
+
+  if (unauthorized) {
+    return NextResponse.rewrite(new URL("/unauthorized", req.url));
   }
+
   return NextResponse.next();
 }
+
+const validateIP = (ip: string) => {
+  return allowedIPs.includes(ip);
+};
+const validateHostname = (hostname: string) => {
+  return hostname === "localhost" || hostname === "127.0.0.1";
+};
+
+const validateToken = (token: string): boolean => {
+  const tokenParts = token.split(" ");
+  const isValidFormat = tokenParts.length === 2 && tokenParts[0] === "Bearer";
+  const providedToken = isValidFormat ? tokenParts[1] : token;
+
+  try {
+    const decoded = jwt.verify(providedToken, JWT_SECRET);
+    return !!decoded;
+  } catch {
+    return false;
+  }
+};
+
+export default verifyAuth;
